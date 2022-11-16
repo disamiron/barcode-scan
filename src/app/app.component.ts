798011734b96cd767dc938f21b81b53e46a5ee66
disamiron <denis.mironov@ddgcorp.ru>
2022-11-16 21:06:58 +0300

    test new lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+} from "@angular/core";
+import { QuaggaJSResultObject } from "@ericblade/quagga2";
 import { BarcodeScannerLivestreamComponent } from "ngx-barcode-scanner";
+import Quagga from "@ericblade/quagga2";
+// import { getMainBarcodeScanningCamera } from './camera-access';
 
 @Component({
   selector: "app-root",
@@ -10,14 +18,133 @@ export class AppComponent implements AfterViewInit {
   @ViewChild(BarcodeScannerLivestreamComponent)
   public barcodeScanner!: BarcodeScannerLivestreamComponent;
   public barcodeValue: any;
-
+  public started = false;
   title = "barcode-scan";
 
   public ngAfterViewInit() {
-    this.barcodeScanner.start();
+    // this.barcodeScanner.start();
+    // this.barcodeScanner._valueChanges.subscribe((v) => {
+    //   console.log(v);
+    // });
+    this.initializeScanner();
   }
 
-  public onValueChanges(result: any) {
-    this.barcodeValue = result.codeResult.code;
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+  private initializeScanner(): Promise<void> {
+    if (
+      !navigator.mediaDevices ||
+      !(typeof navigator.mediaDevices.getUserMedia === "function")
+    ) {
+      // this.errorMessage = 'getUserMedia is not supported. Please use Chrome on Android or Safari on iOS';
+      this.started = false;
+      // return Promise.reject(this.errorMessage);
+    }
+
+    // enumerate devices and do some heuristics to find a suitable first camera
+    return Quagga.CameraAccess.enumerateVideoDevices()
+      .then((mediaDeviceInfos) => {
+        return this.initializeScannerWithDevice(undefined);
+      })
+      .catch((error) => {
+        // this.errorMessage = `Failed to enumerate devices: ${error}`;
+        this.started = false;
+      });
   }
+
+  onBarcodeScanned(code: string) {
+    console.log(code);
+    this.barcodeValue = code;
+    // ignore duplicates for an interval of 1.5 seconds
+    // const now = new Date().getTime();
+    // if (code === this.lastScannedCode
+    //   && ((this.lastScannedCodeDate !== undefined) && (now < this.lastScannedCodeDate + 1500))) {
+    //   return;
+    // }
+
+    // only accept articles from catalogue
+    // let article = this.catalogue.find(a => a.ean === code);
+    // if (!article) {
+    //   if (this.acceptAnyCode) {
+    //     article = this.createUnknownArticle(code);
+    //   } else {
+    //     return;
+    //   }
+    // }
+    // this.shoppingCart.addArticle(article);
+    // this.items = this.shoppingCart.contents;
+    // this.totalPrice = this.shoppingCart.totalPrice;
+
+    // this.lastScannedCode = code;
+    // this.lastScannedCodeDate = now;
+    // this.beepService.beep();
+    this.changeDetectorRef.detectChanges();
+  }
+
+  private initializeScannerWithDevice(
+    preferredDeviceId: string | undefined
+  ): Promise<void> {
+    console.log(`Initializing Quagga scanner...`);
+
+    const constraints: MediaTrackConstraints = {};
+    if (preferredDeviceId) {
+      // if we have a specific device, we select that
+      constraints.deviceId = preferredDeviceId;
+    } else {
+      // otherwise we tell the browser we want a camera facing backwards (note that browser does not always care about this)
+      constraints.facingMode = "environment";
+    }
+
+    return Quagga.init(
+      {
+        inputStream: {
+          type: "LiveStream",
+          constraints,
+          area: {
+            // defines rectangle of the detection/localization area
+            top: "25%", // top offset
+            right: "10%", // right offset
+            left: "10%", // left offset
+            bottom: "25%", // bottom offset
+          },
+          target: document.querySelector("#scanner-container") ?? undefined,
+        },
+        decoder: {
+          readers: ["ean_reader"],
+          multiple: false,
+        },
+        // See: https://github.com/ericblade/quagga2/blob/master/README.md#locate
+        locate: false,
+      },
+      (err) => {
+        if (err) {
+          console.error(`Quagga initialization failed: ${err}`);
+          // this.errorMessage = `Initialization error: ${err}`;
+          this.started = false;
+        } else {
+          console.log(`Quagga initialization succeeded`);
+          Quagga.start();
+          this.started = true;
+          this.changeDetectorRef.detectChanges();
+          Quagga.onDetected((res) => {
+            if (res.codeResult.code) {
+              this.onBarcodeScanned(res.codeResult.code);
+            }
+          });
+        }
+      }
+    );
+  }
+
+  // public onValueChanges(result: QuaggaJSResultObject) {
+  //   console.log(result);
+
+  //   this.barcodeValue = result.codeResult.code;
+  // }
+  // public onStarted(started: any) {
+  //   console.log(started);
+  // }
+
+  // public restart() {
+  //   console.log(this.barcodeScanner);
+  // }
 }
