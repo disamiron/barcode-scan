e32fee25aaced2a1a919665db9988a64a1861a5e
disamiron <denis.mironov@ddgcorp.ru>
2022-11-16 19:34:14 +0300

    plug Barscan to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { BarcodeScannerLivestreamComponent } from "ngx-barcode-scanner";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'barcode-scan';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(BarcodeScannerLivestreamComponent)
+  public barcodeScanner!: BarcodeScannerLivestreamComponent;
+  public barcodeValue: any;
+
+  title = "barcode-scan";
+
+  public ngAfterViewInit() {
+    this.barcodeScanner.start();
+  }
+
+  public onValueChanges(result: any) {
+    this.barcodeValue = result.codeResult.code;
+  }
 }
