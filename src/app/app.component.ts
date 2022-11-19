edd92b42abbf69896e201f0f22ab0e21f20b806d
disamiron <denis.mironov@ddgcorp.ru>
2022-11-19 12:08:33 +0300

    update logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,93 +21,25 @@ export class AppComponent implements AfterViewInit {
 
   public started = false;
 
-  public errorMessage: string | null = null;
-
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   public ngAfterViewInit() {
-    // this.initializeScanner();
     this.barcodeScanner.start();
   }
 
   public onValueChanges(result: QuaggaJSResultObject) {
-    this.barcodeValue = result.codeResult.code;
-  }
-  public onStarted() {
-    console.log("started");
-  }
-  public initializeScanner(): Promise<void> {
-    this.barcodeValue = null;
-    if (
-      !navigator.mediaDevices ||
-      !(typeof navigator.mediaDevices.getUserMedia === "function")
-    ) {
-      this.errorMessage =
-        "getUserMedia is not supported. Please use Chrome on Android or Safari on iOS";
+    if (result.codeResult.code) {
+      this.barcodeValue = result.codeResult.code;
+      this.barcodeScanner.stop();
       this.started = false;
     }
-
-    return Quagga.CameraAccess.enumerateVideoDevices()
-      .then(() => {
-        return this.initializeScannerWithDevice(undefined);
-      })
-      .catch((error) => {
-        this.errorMessage = `Failed to enumerate devices: ${error}`;
-        this.started = false;
-      });
   }
 
-  private initializeScannerWithDevice(
-    preferredDeviceId: string | undefined
-  ): Promise<void> {
-    const constraints: MediaTrackConstraints = {};
-
-    if (preferredDeviceId) {
-      constraints.deviceId = preferredDeviceId;
-    } else {
-      constraints.facingMode = "environment";
-    }
-
-    return Quagga.init(
-      {
-        inputStream: {
-          type: "LiveStream",
-          constraints,
-          area: {
-            top: "25%",
-            right: "10%",
-            left: "10%",
-            bottom: "25%",
-          },
-          target: document.querySelector("#scanner-container")!,
-        },
-        decoder: {
-          readers: ["ean_reader"],
-          multiple: false,
-        },
-        locate: false,
-      },
-      (err) => {
-        if (err) {
-          this.errorMessage = `Initialization error: ${err}`;
-          this.started = false;
-        } else {
-          Quagga.start();
-          this.started = true;
-          this.changeDetectorRef.detectChanges();
-          Quagga.onDetected((res) => {
-            if (res.codeResult.code) {
-              this.onBarcodeScanned(res.codeResult.code);
-            }
-          });
-        }
-      }
-    );
+  public barcodeScanStarted() {
+    this.started = true;
   }
-  onBarcodeScanned(code: string) {
-    this.barcodeValue = code;
-    this.changeDetectorRef.detectChanges();
-    this.started = false;
-    Quagga.stop();
+
+  public newScan() {
+    this.barcodeScanner.start();
   }
 }
